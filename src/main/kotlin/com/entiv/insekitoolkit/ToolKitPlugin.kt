c0c8c0b739de1e7ddb91dda5072635b00267ae95
EnTIv <38716516+EnTIv@users.noreply.github.com>
2023-02-01 10:56:16 +0800

    update

--- src/main/kotlin/com/entiv/insekitoolkit/ToolKitPlugin.kt
+++ src/main/kotlin/com/entiv/insekitoolkit/ToolKitPlugin.kt
@@ -9,13 +9,7 @@ class ToolKitPlugin : InsekiPlugin() {
     }
 
     private fun loadModules() {
-        moduleManager.load(简易死亡惩罚)
-        moduleManager.load(空岛基岩处理器)
-        moduleManager.load(虚空掉落检查)
-        moduleManager.load(地狱门处理器)
-        moduleManager.load(磁盘空间检查)
-        moduleManager.load(游戏规则管理)
         moduleManager.load(绑定诅咒补丁)
-        moduleManager.load(烟花补丁)
+        moduleManager.load(展示框保护)
     }
 }
--- src/main/kotlin/com/entiv/insekitoolkit/展示框保护.kt
+++ src/main/kotlin/com/entiv/insekitoolkit/展示框保护.kt
@@ -1,6 +1,7 @@
 package com.entiv.insekitoolkit
 
 import com.entiv.core.module.PluginModule
+import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.ProjectileHitEvent
@@ -9,7 +10,8 @@ object 展示框保护 : ConfigModule, Listener {
 
     @EventHandler
     private fun onProjectileHit(event: ProjectileHitEvent) {
-        println(event.hitEntity)
+        if (event.hitEntity?.type == EntityType.ITEM_FRAME) {
+            event.isCancelled = true
+        }
     }
-
 }
